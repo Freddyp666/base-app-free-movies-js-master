6c64979d13236d1310989ad7cab0942245491b26
Freddyp666 <pat-.-freddy@outlook.com>
2025-07-11 21:56:52 -0500

    Finished search movies

--- api.js
+++ api.js
@@ -1,8 +1,18 @@
 
-let api_key ='ea5e6f0ed4fe32cab0924dea5529c05e'; // Your API key here
-let api_url ='https://api.themoviedb.org/3/search/movie'
+let api_key = 'ea5e6f0ed4fe32cab0924dea5529c05e'; // Your API key here
+let api_url = 'https://api.themoviedb.org/3/search/movie'
 
 
 function getMovies(queryInput) {
-    fetch (`${api_url}?api_key=${api_key}&query=${searchInput}`)
+
+   let url = `${api_url}?api_key=${api_key}&query=${queryInput}`;
+
+   return fetch(url)
+      .then(response => response.json())
+      .then(data => data.results)
+      .catch(error => {
+         console.error('Error fetching movies:', error);
+         throw error;
+      });
+
 }
--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-
-
 //Button to search for movies
-document.getElementById('searchButton').addEventListener('click',searchMovies);
+document.getElementById('searchButton').addEventListener('click', searchMovies);
 
-
+    let resultsContainer = document.getElementById('results');
+     // Clear previous results
 // Function to search for movies
 function searchMovies() {
-    let searchInput =document.getElementById('searchInput').value;
+    resultsContainer.innerHTML = 'Cargando...';
+    let searchInput = document.getElementById('searchInput').value;
+
+    if (searchInput.trim() === '') {
+        alert('Please enter a movie name');
+        return;
+    } else {
+        getMovies(searchInput)
+            .then(movies => displayMovies(movies))
+            .catch(error => {
+                console.error('Error:', error);
+                alert('An error occurred while fetching movies. Please try again later.');
+            });
+
+    }
 
 }
+
+// Function to display movies in the UI
+function displayMovies(movies) {
+    resultsContainer.innerHTML = ''; // Clear previous results
+
+
+    if (movies.length === 0) {
+        resultsContainer.innerHTML = '<p>No movies found.</p>';
+        return;
+    }
+
+    movies.forEach(movie => {
+        let movieElement = document.createElement('div');
+        movieElement.className = 'movie';
+        movieElement.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} Poster" class="movie-poster" />
+
+            <h3>${movie.title}</h3>
+            <p>Release Date: ${movie.release_date}</p>
+            <p>Overview: ${movie.overview}</p>
+            <p>Rating: ${movie.vote_average}</p>
+            <p>Popularity: ${movie.popularity}</p>
+            <p>Language: ${movie.original_language}</p>
+        `;
+        resultsContainer.appendChild(movieElement);
+
+    })
+}
+
+// Function to clear search results
+document.getElementById('clearButton').addEventListener('click', () => {
+    document.getElementById('searchInput').value = '';
+    resultsContainer.innerHTML = '';
+});
